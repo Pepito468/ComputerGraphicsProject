50e1d8bdaa1f1f905e7e17bc5ae5d6947a2d390e
mssuperlol <mssuperlol@gmail.com>
2026-08-25 10:11:01 +0200

    Added new generateTexture methods

--- source/src/engine/rendering/Texture.hpp
+++ source/src/engine/rendering/Texture.hpp
@@ -10,7 +10,10 @@ public:
         std::vector<uint8_t> pixels; // RGBA8
     };
 
-    static TextureData generateUITexture(int width = 256, int height = 256) {
+    /**
+     * Generates a texture of the given dimensions of a uniform rectangle of the given color
+     * */
+    static TextureData generateTexture(int width = 256, int height = 256, int r_in = 150, int g_in = 55, int b_in = 38, int a_in = 255) {
         TextureData texture;
         texture.width = width;
         texture.height = height;
@@ -18,18 +21,40 @@ public:
 
         for (int y = 0; y < height; ++y) {
             for (int x = 0; x < width; ++x) {
-                // Simple procedural variation
-                float noise = 0.85f + 0.15f * std::sin(x * 0.11f) * std::sin(y * 0.07f);
-
-                uint8_t r = static_cast<uint8_t>(150 * noise);
-                uint8_t g = static_cast<uint8_t>(55 * noise);
-                uint8_t b = static_cast<uint8_t>(38 * noise);
-
-                setPixel(texture, x, y, r, g, b);
+                setPixel(texture, x, y, static_cast<uint8_t>(r_in), static_cast<uint8_t>(g_in), static_cast<uint8_t>(b_in), static_cast<uint8_t>(a_in));
             }
         }
 
         return texture;
+    }
+
+    /**
+     * Generates a texture of the given dimensions of a rectangle of a given color with some noise
+     * */
+    static TextureData generateTextureWithNoise(int width = 256, int height = 256, int r_in = 150, int g_in = 55, int b_in = 38, int a_in = 255, float constNoise = 0.85f, float varNoise = 0.15f, float xNoise = 0.11f, float yNoise = 0.07f) {
+        TextureData texture;
+        texture.width = width;
+        texture.height = height;
+        texture.pixels.resize(width * height * 4);
+        float noise;
+
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                noise = constNoise + varNoise * std::sin(x * xNoise) * std::sin(y * yNoise);
+
+                uint8_t r = static_cast<uint8_t>(r_in * noise);
+                uint8_t g = static_cast<uint8_t>(g_in * noise);
+                uint8_t b = static_cast<uint8_t>(b_in * noise);
+
+                setPixel(texture, x, y, r, g, b, static_cast<uint8_t>(a_in));
+            }
+        }
+
+        return texture;
+    }
+
+    static TextureData generateMenuBackgroundTint(int width, int height) {
+        return generateTexture(width, height, 255, 255, 255, 100);
     }
 
 private:
