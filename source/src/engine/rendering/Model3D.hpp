02af0cb92db5c18790ea04a92696777da43ea144
andrearuben003 <andrearuben003@gmail.com>
2026-09-01 16:37:27 +0200

    Small change

--- source/src/engine/rendering/Model3D.hpp
+++ source/src/engine/rendering/Model3D.hpp
@@ -152,7 +152,6 @@ public:
             Anim[i].init(assets[i]);
         }
         AB.init(segments);
-        AB.Start(2, 0.0f); //TODO: temp
         SKA.init(Anim.data(), assets.size(), armatureName, 0);
     }
 
